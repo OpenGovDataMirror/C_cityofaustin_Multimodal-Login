b350b1b82f5759d8c9dc09da6ee1a5c09805182c
ep-infosec <ep-infosec@users.noreply.github.com>
2022-12-14 12:44:55 +0200

    init

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+cd ~/PROD/MYPASS-OAUTH/multimodal-login
+
+git pull
+
+npm install
+npm run build
+
+# need to do a couple extra steps to get dist correct for node
+cd dist
+cp ../.env ./
+cp ../package.json ./
+npm install --production
+
+
+cd ~/PROD/MYPASS-OAUTH/multimodal-login
+
+pm2 restart oauth
+
+echo ~~FINISHED~~
